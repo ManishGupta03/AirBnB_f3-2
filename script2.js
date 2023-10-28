22d27d5b5a31aff00eb64422a98b9fa58577fc5e
Manish Gupta <117648576+ManishGupta03@users.noreply.github.com>
2023-10-28 19:37:17 +0000

    AirBnb #rd week

--- script2.js
+++ script2.js
@@ -41,7 +41,130 @@ function createListingCard(data) {
                 `;
         listingsContainer.append(listingCard);
     });
+    // Create a button to open the booking cost breakdown modal
+    const bookingButton = document.createElement("button");
+    bookingButton.innerText = "View Cost Breakdown";
+    bookingButton.addEventListener("click", () => {
+        showBookingCostBreakdown(listing);
+    });
+
+    // Append the button to the listing card
+    listingCard.appendChild(bookingButton);
+
+    return listingCard;
 };
+
+
+function showBookingCostBreakdown(listing) {
+    // Create a modal dialog
+    const modal = document.createElement("div");
+    modal.classList.add("modal");
+
+    // Add the content for the modal (booking cost breakdown)
+    const content = document.createElement("div");
+    content.classList.add("modal-content");
+    content.innerHTML = `
+        <h2>Booking Cost Breakdown</h2>
+        <p>Base Rate: $${listing.baseRate}</p>
+        <p>Additional Fees: $${listing.additionalFees}</p>
+        <p>Total Cost: $${listing.baseRate + listing.additionalFees}</p>
+    `;
+
+    // Create a close button
+    const closeButton = document.createElement("span");
+    closeButton.classList.add("close");
+    closeButton.innerHTML = "&times;";
+    closeButton.addEventListener("click", () => {
+        // Close the modal when the close button is clicked
+        modal.style.display = "none";
+    });
+
+    // Append the close button to the modal content
+    content.appendChild(closeButton);
+
+    // Append the content to the modal
+    modal.appendChild(content);
+
+    // Append the modal to the document's body
+    document.body.appendChild(modal);
+
+    // Display the modal
+    modal.style.display = "block";
+}
+
+
+
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 37.7749, lng: -122.4194 }, // Example coordinates (San Francisco)
+        zoom: 10, // Example zoom level
+    });
+
+    // Now, you have a map ready to display markers.
+}
+
+
+
+function createListingCard(listing) {
+    // ... Your existing code to create the listing card ...
+
+    // Create a marker for the listing on the map
+    const marker = new google.maps.Marker({
+        position: { lat: listing.latitude, lng: listing.longitude },
+        map: map, // Assuming 'map' is the variable holding your Google Map instance
+        title: listing.title, // Display the title when the marker is clicked
+    });
+
+    // Add an event listener to open an info window when the marker is clicked
+    marker.addListener("click", function() {
+        const infowindow = new google.maps.InfoWindow({
+            content: listing.title,
+        });
+        infowindow.open(map, marker);
+    });
+
+    // ... Continue creating your listing card ...
+
+    return listingCard;
+}
+
+
+function createListingCard(listing) {
+    // ... Your existing code to create the listing card ...
+
+    // Create a "Get Directions" button
+    const directionsButton = document.createElement("button");
+    directionsButton.innerText = "Get Directions";
+    directionsButton.addEventListener("click", function() {
+        openDirections(listing.location);
+    });
+
+    // Append the button to the listing card
+    listingCard.appendChild(directionsButton);
+
+    // ... Continue creating your listing card ...
+
+    return listingCard;
+}
+
+// Function to open Google Maps directions in a new tab
+function openDirections(location) {
+    // Construct the Google Maps directions URL using the latitude and longitude of the property
+    const url = `https://www.google.com/maps/dir//${location.latitude},${location.longitude}`;
+
+    // Open the URL in a new tab
+    window.open(url, "_blank");
+}
+
+// Example usage
+const sampleListing = {
+    // ... Your listing data, including the 'location' property with latitude and longitude ...
+};
+
+const listingCard = createListingCard(sampleListing);
+
+// Append the listing card to your webpage or container
+// document.getElementById('your-listings-container').appendChild(listingCard);
 
 function hotelDetail(data) {
     console.log(data);
